56bdd2e0025f41c79516d67d91640a278aa3e569
Calvin Ren <calvin.qut@gmail.com>
2023-04-12 17:42:13 +1000

    add demo ui

---
+++ packages/demo_ui/test/match_golden.dart
@@ -0,0 +1,47 @@
+import 'package:alchemist/alchemist.dart';
+import 'package:demo_theme/demo_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const _theme = UiTheme();
+const _columns = 2;
+
+class Scenario {
+  Scenario({
+    required this.widget,
+    this.name,
+  });
+
+  final Widget widget;
+  final String? name;
+}
+
+/// Golden helper.
+Future<void> matchGolden({
+  required final String name,
+  required final List<Scenario> scenarios,
+  final Future<void> Function(WidgetTester) pumpBeforeTest = onlyPumpAndSettle,
+  final Future<Future<void> Function()?> Function(WidgetTester)?
+      whilePerforming,
+}) async =>
+    goldenTest(
+      name,
+      fileName: name.replaceAll(' ', '_').toLowerCase(),
+      pumpBeforeTest: pumpBeforeTest,
+      whilePerforming: whilePerforming,
+      builder: () => GoldenTestGroup(
+        columns: _columns,
+        children: [
+          for (final scenario in scenarios)
+            GoldenTestScenario(
+              name: '${scenario.name ?? name} light',
+              child: Theme(data: _theme.light, child: scenario.widget),
+            ),
+          for (final scenario in scenarios)
+            GoldenTestScenario(
+              name: '${scenario.name ?? name} dark',
+              child: Theme(data: _theme.dark, child: scenario.widget),
+            ),
+        ],
+      ),
+    );
