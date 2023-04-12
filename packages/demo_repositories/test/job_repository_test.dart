1e813142e6ee8ddcc5092e1d0925716f61dc0b9e
Calvin Ren <calvin.qut@gmail.com>
2023-04-12 20:15:03 +1000

    add demo repositories

---
+++ packages/demo_repositories/test/job_repository_test.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: avoid_print
+
+import 'package:demo_http_api/src/jobs/job_api.dart';
+import 'package:demo_http_api/src/util/api_client.dart';
+import 'package:demo_repositories/src/job_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Job repository', () {
+    const apiKey = String.fromEnvironment('API_KEY');
+
+    final apiClient = ApiClientImpl(
+      baseURL: 'localhost',
+      port: 8080,
+      headers: () async {
+        /// TODO: Refresh token if expired
+
+        return <String, String>{
+          'Content-Type': 'application/json',
+          'api-key': apiKey,
+        };
+      },
+    );
+    test('should get jobs from the internet', () async {
+      final jobRepository = JobRepository(jobApi: JobApi(apiClient: apiClient));
+      final jobs = await jobRepository.fetchJobs(searchText: 'flutter');
+      jobs.when(
+        left: (failure) {
+          print(failure.message);
+        },
+        right: (jobs) {
+          expect(jobs.isNotEmpty, true);
+          print(jobs);
+          print(jobs.length);
+        },
+      );
+    });
+  });
+}
